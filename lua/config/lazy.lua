37168580eaf322cd16b84a58cc3ef64a75be5ab1
githubzilla <githubzilla@gmail.com>
2025-04-04 10:10:10 +0800

    1. Add Trouble.nvim 2. Add Surrund.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -144,9 +144,60 @@ return require("lazy").setup({
             "zbirenbaum/copilot.lua" -- for providers='copilot'
         },
         config = require("plugins.avante")
-    }
+    },
 
-}, {
+    -- Trouble
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
+
+    -- surround
+    {
+      "kylechui/nvim-surround",
+      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+    }
+}, 
+{
     install = {colorscheme = {"material"}},
     checker = {enabled = true, notify = false},
     change_detection = {notify = false}
