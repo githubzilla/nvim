2cefe2e90e0ce1fb16bb08bfc61ee6598a86f037
githubzilla <githubzilla@gmail.com>
2025-07-24 18:12:40 +0800

    using clang-format-18

--- lua/plugins/clang-format.lua
+++ lua/plugins/clang-format.lua
@@ -1,7 +1,7 @@
 return function()
 -- Set the clang-format command and enable style file detection
 vim.g["clang_format#detect_style_file"] = 1
-vim.g["clang_format#command"] = "/usr/bin/clang-format-17"
+vim.g["clang_format#command"] = "/usr/bin/clang-format-18"
 
 -- Map <leader>cf to the ClangFormat command
 vim.api.nvim_set_keymap('n', '<leader>cf', '<cmd>ClangFormat<cr>', { noremap = true, silent = true })
